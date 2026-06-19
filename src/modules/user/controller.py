48213320fbbe4d462b579d65c1375aa3a1af6a56
zer4c <pabloazurduy@gmail.com>
2026-06-18 20:44:57 -0400

    feat: added controller for user module

--- src/modules/user/controller.py
+++ src/modules/user/controller.py
@@ -1,2 +1,39 @@
-class UserController():
-    pass
+from src.modules.user.services import UserService
+from src.core.database import SessionDep
+from src.modules.user.schemas import UserCreate, UserPatch
+from src.core.response_model import IResponse
+from fastapi import HTTPException
+
+
+class UserController:
+    @staticmethod
+    async def get_all(session: SessionDep):
+        users = await UserService.get_all(session)
+        return IResponse(code=200, message="users retrieved", data=users)
+
+    @staticmethod
+    async def get_by_id(id: int, session: SessionDep):
+        user = await UserService.get_by_id(id, session)
+        if not user:
+            raise HTTPException(status_code=404, detail="user not found")
+        return IResponse(code=200, message="user retrieved", data=user)
+
+    @staticmethod
+    async def create_user(payload: UserCreate, session: SessionDep):
+        user = await UserService.create_user(payload, session)
+        return IResponse(code=201, message="user created", data=user)
+
+    @staticmethod
+    async def update_user(id: int, payload: UserPatch, session: SessionDep):
+        user = await UserService.get_by_id(id, session)
+        if not user:
+            raise HTTPException(status_code=404, detail="user not found")
+        user_updated = await UserService.update_user(id, payload, session)
+        return IResponse(code=200, message="user updated", data=user_updated)
+
+    @staticmethod
+    async def delete_user(id: int, session: SessionDep):
+        user = await UserService.get_by_id(id, session)
+        if not user:
+            raise HTTPException(status_code=404, detail="user not found")
+        await UserService.delete_user(id, session)
